ce55162e65462bf55ffd285b1bb9690efd512415
samyak-270605 <samyak272005@gmail.com>
2026-04-04 22:29:53 +0530

    attempted 1090 div4, solved till D

--- sam1.cpp
+++ sam1.cpp
@@ -1,63 +1,61 @@
 // Input:-
 
-// 6
-// 1 1
-// 3 3 3
-// 1 7
-// 4 2 5
-// 2 4
-// 1 2 3
-// 2 2 4
-// 5 8
-// 12 1 11
-// 10 1 4
-// 1 1 3
-// 1 2 5
-// 2 1 7
-// 1 1000000000000000000
-// 1000000 4 654321
-// 1 10
-// 2 2 1
+// 2
+// 3
+// 5
+// 10000
 
 // Output:-
 
-// 0
-// 1
-// -1
+// 1 6 2
+// 134 67 69 207 414
+
+
+// Input:-
+
 // 2
-// 298892990032
 // 3
+// 5
+// 10000
+
+// Output:-
+
+// 1 6 2
+// 134 67 69 207 414
+
 
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
 
-
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
+    
+    // Precompute primes up to ~105000 (need ~10001 primes)
+    vector<int> primes;
+    vector<bool> sieve(105000, true);
+    for(int i = 2; i < 105000 && primes.size() <= 10001; i++){
+        if(sieve[i]){
+            primes.push_back(i);
+            for(int j = 2*i; j < 105000; j += i) sieve[j] = false;
+        }
+    }
+    
     int t;
     cin >> t;
-
+    
     while(t--){
-            ll n,x;
-            cin>>n>>x;
-            vector<ll> a(n),b(n),c(n);
-            for(ll i=0; i<n; i++) cin>>a[i]>>b[i]>>c[i];
-
-            ll start = 0;
-            ll mx = LLONG_MIN;
-            for(ll i=0; i<n; i++){
-                mx = max(mx,a[i] * b[i] - c[i]);
-                start += a[i] * (b[i] - 1);
-            }
-
-            x -= start;
-            if(x <= 0) cout<<0;
-            else if(mx <= 0) cout<<-1;
-            else cout<<(x + mx - 1) / mx;
-            cout<<'\n';
+        int n;
+        cin >> n;
+        
+        // a[i] = prime[i] * prime[i+1]
+        // gcd(a[i], a[i+1]) = prime[i+1], all distinct
+        for(int i = 0; i < n; i++){
+            cout << (ll)primes[i] * primes[i+1];
+            if(i < n-1) cout << " ";
         }
+        cout << "\n";
+    }
     return 0;
 }
