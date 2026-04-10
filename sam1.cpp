317aab0e37629ce65f903e9b188bbec6c8e26e75
samyak-270605 <samyak272005@gmail.com>
2026-04-10 08:16:24 +0530

    2218F div4 solved

--- sam1.cpp
+++ sam1.cpp
@@ -1,27 +1,42 @@
 // Input:-
 
-// 2
-// 3
-// 5
-// 10000
+// 7
+// 1 1
+// 2 1
+// 0 3
+// 3 4
+// 0 2
+// 1 0
+// 4 7
 
 // Output:-
 
-// 1 6 2
-// 134 67 69 207 414
-
-
-// Input:-
-
-// 2
-// 3
-// 5
-// 10000
-
-// Output:-
-
-// 1 6 2
-// 134 67 69 207 414
+// YES
+// 1 2
+// NO
+// YES
+// 1 2
+// 1 3
+// YES
+// 1 2
+// 2 3
+// 3 4
+// 4 5
+// 5 6
+// 6 7
+// NO
+// NO
+// YES
+// 1 2
+// 2 3
+// 3 4
+// 4 5
+// 4 11
+// 2 6
+// 6 7
+// 7 8
+// 2 9
+// 9 10
 
 
 #include<bits/stdc++.h>
@@ -32,30 +47,31 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-    // Precompute primes up to ~105000 (need ~10001 primes)
-    vector<int> primes;
-    vector<bool> sieve(105000, true);
-    for(int i = 2; i < 105000 && primes.size() <= 10001; i++){
-        if(sieve[i]){
-            primes.push_back(i);
-            for(int j = 2*i; j < 105000; j += i) sieve[j] = false;
-        }
-    }
-    
     int t;
     cin >> t;
     
     while(t--){
-        int n;
-        cin >> n;
+        int x,y;
+        cin>>x>>y;
         
-        // a[i] = prime[i] * prime[i+1]
-        // gcd(a[i], a[i+1]) = prime[i+1], all distinct
-        for(int i = 0; i < n; i++){
-            cout << (ll)primes[i] * primes[i+1];
-            if(i < n-1) cout << " ";
+        int n = x + y;
+        int d = y - x;
+
+        if((x == 0 && n%2 == 0) || n/2 < x){
+            cout<<"NO"<<endl;
+            continue;
         }
-        cout << "\n";
+
+        cout<<"YES"<<endl;
+
+        int endNode = 2*x + (d % 2);
+        for(int i=2; i<=endNode; i++){
+            cout<<i-1<<" "<<i<<endl;
+        }
+
+        for(int i=endNode+1; i<=n; i++){
+            cout<<endNode<<" "<<i<<endl;
+        }
     }
     return 0;
 }
