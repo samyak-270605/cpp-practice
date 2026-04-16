4d82f04de85566844fcd582513d76ea985a6963b
samyak-270605 <samyak272005@gmail.com>
2026-04-16 21:03:37 +0530

    2210-C1 solved

--- sam1.cpp
+++ sam1.cpp
@@ -1,30 +1,23 @@
-// Input:-
-
+// InputCopy
+// 4
 // 7
-// 4 3
-// 0 1 2 0
-// 8 4
-// 0 1 2 3 1 2 0 1
-// 9 5
-// 1 0 1 3 4 3 2 1 0
-// 15 14
-// 3 0 1 2 3 4 5 6 7 8 9 10 11 12 13
-// 5 5
-// 4 3 0 1 2
-// 5 2
-// 0 1 1 1 0
-// 3 2
-// 0 1 1
-
-// Output:-
-
-// 2
-// 0
-// 1920
-// 138007136
+// 1 2 3 4 5 6 7
+// 1 2 3 4 5 6 7
+// 3
+// 67 67 67
+// 67 67 67
+// 6
+// 8 10 10 12 12 14
+// 8 10 10 12 12 14
 // 8
+// 2 4 8 16 32 64 128 256
+// 2 4 8 16 32 64 128 256
+// OutputCopy
+// 6
 // 0
-// 0
+// 2
+// 1
+
 
 
 #include<bits/stdc++.h>
@@ -39,41 +32,25 @@ int main(){
     cin >> t;
     
     while(t--){
-        ll n,m;
-        cin>>n>>m;
-        vector<ll> b(n);
+        ll n;
+        cin>>n;
+        vector<ll> arr(n),b(n);
+        for(auto &x : arr) cin>>x;
         for(auto &x : b) cin>>x;
-        ll mod = 676767677;
+        ll res = 0;
 
-        map<ll,ll> f,fq;
-        for(ll i : b) f[i]++;
-
-        ll sum = 0;
-        for(ll i=0; i<m; i++){
-            sum += f[i];
-            fq[i+1] = sum;
+        for(ll i=1; i<n-1; i++){
+           ll a = __gcd(arr[i], arr[i-1]);
+           ll c = __gcd(arr[i], arr[i+1]);
+           ll lcm = (a * c)/__gcd(a,c);
+           if(lcm < arr[i]) res++;
         }
-
-        ll ans = 1;
-        for(ll i=0; i<n; i++){
-            if(b[i] == 0) continue;
-            ll mn = INT_MAX;
-            if(i-1 >=0) mn = min(mn,b[i-1]);
-            if(i+1 < n) mn = min(mn,b[i+1]);
-            
-            if(mn >= b[i]){
-                ans = 0;
-                break;
-            }
-
-            if(mn == b[i]-1)
-                ans = ans * fq[b[i]]%mod;
-            else
-                ans = ans * f[b[i]-1]%mod;
-        }
-
-        if(ans == 0) cout<<0<<endl;
-        else cout<<ans<<endl;
+        
+            if(__gcd(arr[0], arr[1]) < arr[0]) res++;
+            if(__gcd(arr[n-1], arr[n-2]) < arr[n-1]) res++;
+        
+        cout<<res<<endl;
     }
+
     return 0;
 }
