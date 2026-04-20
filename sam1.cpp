4d32556ff675c408bdb111fdcf5ed02b5c101848
samyak-270605 <samyak272005@gmail.com>
2026-04-20 19:20:48 +0530

    2174A

--- sam1.cpp
+++ sam1.cpp
@@ -1,27 +1,59 @@
 // Input:-
 
-// 4
-// 2 0
-// 1 1
-// 6 7
-// 1 3
+// 3
+// dcbe
+// bedbaecfc
+// babadab
+// abacabadabacaba
+// babaisyou
+// flagiswin
 
 // Output:-
 
-// 2
-// 1 1
-// 1
-// 1 -1
-// 1
-// -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1
-// 2
-// -1 -1 -1 1
-
+// abcdcbeef
+// aaaaabababccdab
+// Impossible
 
 
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
+
+void solve(string s, string t){
+    vector<int> alpha(26,0);
+    for(auto ch : t) alpha[ch - 'a']++;
+    for(auto ch : s){
+        if(alpha[ch - 'a'] == 0){
+            cout<<"Impossible"<<endl;
+            return;
+        }
+        alpha[ch - 'a']--;
+    }
+
+    string temp = "";
+    //int start = 0;
+    for(auto ch : s){
+        int end = ch - 'a';
+        for(int i=0; i<end; i++){
+            while(alpha[i]){
+                temp += 'a' + i;
+                alpha[i]--;
+            }
+        }
+        temp += ch;
+        //start = end + 1;
+    }
+
+    for(int i=0; i<26; i++){
+        while(alpha[i]){
+            temp += 'a' + i;
+            alpha[i]--;
+        }
+    }
+
+    cout<<temp<<endl;
+    return;
+}
 
 int main(){
     ios::sync_with_stdio(false);
@@ -31,28 +63,10 @@ int main(){
     cin >> t;
     
     while(t--){
-        ll x,y;
-        cin>>x>>y;
-
-        vector<ll> arr;
-        ll mod = 676767677;
-        for(auto i=0; i<x; i++) arr.push_back(1);
-        for(auto i=0; i<y; i++) arr.push_back(-1);
-        ll diff = abs(x - y); // total sum of array
-        if(diff == 0) cout<<1<<endl;
-        else{
-            int ans = 0;
-            for(int i = 1; i*i <= diff; i++){
-                if(diff % i == 0){
-                    ans++;
-                    if(diff != i*i) ans++;
-                }
-            }
-            cout<<ans % mod<<endl;
-        }
-
-        for(auto i : arr) cout<<i<<" ";
-        cout<<endl;
+        string s,t;
+        cin>>s;
+        cin>>t;
+        solve(s,t);
     }
 
     return 0;
