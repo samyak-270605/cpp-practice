88d2f3823636e7fe1ddb9c3ad1c325158964df68
samyak-270605 <samyak272005@gmail.com>
2026-05-10 01:43:33 +0530

    2217C div2

--- sam1.cpp
+++ sam1.cpp
@@ -1,52 +1,42 @@
+// 2217C
 // Input:-
 
-// 5
-// 1 1 1
-// 2 0 0
-// 1 1 0
-// 0 0 1000000000
-// 1000000000 1000000000 1000000000
+// 10
+// 1 1 1 1
+// 2 2 1 1
+// 4 2 2 1
+// 6 9 6 7
+// 67 42 42 67
+// 3411 4134 32 23
+// 90234 143124 232 323
+// 69387963 98793214 9791 4324786
+// 985865 578977 899368 447605
+// 1000000000 1000000000 1000000000 1000000000
 
 // Output:-
 
-// 7
-// 5
-// 5
-// 3000000000
-// 7000000000
+// YES
+// YES
+// NO
+// NO
+// YES
+// NO
+// NO
+// NO
+// YES
+// NO
 
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
 
 void solve() {
-    ll t,h,u;
-    cin>>t>>h>>u;
-    ll res = 0;
-    
-    // 1. Greedily pair T and U (most efficient combination)
-    ll pairs = min(t, u);
-    res += pairs * 4;
-    t -= pairs;
-    u -= pairs;
-    
-    // 2. Process whatever is left
-    if (u > 0) {
-        // U's cannot interlock with H's. Both just take 3 rows each.
-        res += u * 3;
-        res += h * 3;
-    } else {
-        // We only have T's and H's left
-        if (t <= 2 * h) {
-            // All T's can interlock into the H's (no overhead)
-            res += 2 * t + 3 * h;
-        } else {
-            // We have more T's than H gaps, triggering a T-chain overhead (+1)
-            res += 2 * t + 3 * h + 1;
-        }
-    }
-
-    cout<<res<<endl;
+    ll n,m,a,b;
+    cin>>n>>m>>a>>b;
+    if(__gcd(n,a) == 1 && __gcd(m,b) == 1 && __gcd(n,m) <= 2)
+        cout<<"YES";
+    else cout<<"NO";
+    cout<<endl;
 }
 
 int main(){
