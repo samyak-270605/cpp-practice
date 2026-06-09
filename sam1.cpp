40374a508d02877ba7cfcb0751f72a67b4c39fb7
samyak-270605 <samyak272005@gmail.com>
2026-06-09 21:31:44 +0530

    2228B solved

--- sam1.cpp
+++ sam1.cpp
@@ -1,81 +1,33 @@
-// 2230C
+// 2228B
 // Input:-
 
-// 11
 // 4
-// 1 1 1 3
-// 3
-// 2 3 4
-// 6
-// 1 1 1 1 3 4
-// 3
-// 1000000000 1000000000 1000000000
-// 3
-// 1 1 2
-// 1
-// 2
-// 2
-// 2 2
-// 3
-// 1 1 1
-// 4
-// 1 1 2 2
-// 3
-// 1 1 4
-// 9
-// 1 1 1 1 1 1 1 1 7
+// 2 1 2 0
+// 4 3 2 1
+// 4 2 3 1
+// 16 8 4 2
 
 // Output:-
 
-// 4
-// 9
-// 8
-// 3000000000
-// 3
-// 0
-// 4
-// 0
-// 4
+// 1
+// 2
+// 2
 // 6
-// 10
 
-#include<bits/stdc++.h>
+#include<bits/stdc++.h>     
 using namespace std;
 using ll = long long;
 
 void solve() {
-    ll n;
-    cin>>n;
-    vector<ll> arr(n);
-    for(auto& x : arr) cin>>x;
-    sort(arr.rbegin(), arr.rend());
-    ll ex = 0;
-    bool exx = false;
-    if(n > 1 && arr[1] == 1 && arr[0] >= 2) exx = true;
+    int n, x1, x2, k;
+    cin>>n>>x1>>x2>>k;
 
-    ll ans = 0;
-    for(auto i : arr){
-        if(i == 1){
-            if(ex > 0){
-                ex--;
-                ans++;
-            }
-            else if(exx){
-                exx = false;
-                ans++;
-            }
-        }
-
-        else{
-            ans += i;
-            if(i < 4) continue;
-
-            ex += 1 + (i-4)/2;
-        }
+    if(n == 2 || n == 3){
+        cout<<1<<endl;
+        return;
     }
-
-    if(ans < 3) ans = 0;
-    cout<<ans<<endl;
+    int res = min(abs(x2-x1), n - abs(x2 - x1)) + k;
+    cout<<res<<endl;
 }
 
 int main(){
