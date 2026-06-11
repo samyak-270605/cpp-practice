8413358e15bcec6912a6854b29a1a9419a29c9bf
samyak-270605 <samyak272005@gmail.com>
2026-06-11 21:56:40 +0530

    2217B solved

--- sam1.cpp
+++ sam1.cpp
@@ -1,76 +1,75 @@
-// 2222B
+// 2217B
 // Input:-
 
-// 6
-// 7 4
-// 1 2 3 4 5 6 7
-// 1 2 3 4
-// 7 4
-// 1 -2 3 4 -5 -6 -7
-// 7 6 5 4
-// 7 5
-// 21 -45 234 -8 423 12 -987
-// 6 6 6 6 6
-// 7 5
-// -21 45 -234 8 -423 -12 987
-// 7 7 7 7 7
-// 7 3
-// -1 2 -3 4 5 6 7
-// 1 2 3
-// 7 3
-// -1 -2 -3 -4 -5 -6 -7
-// 1 2 3
+// 4
+// 3 1
+// 0 1 0
+// 2
+// 5 1
+// 1 1 1 1 1
+// 1
+// 6 1
+// 0 1 0 1 0 1
+// 3
+// 17 1
+// 0 1 1 0 1 1 0 1 0 0 1 0 1 0 1 0 1
+// 5
 
 // Output:-
 
-// 6
-// -20
-// -362
-// -637
 // 2
-// -25
+// 0
+// 4
+// 10
 
 #include<bits/stdc++.h>     
 using namespace std;
 using ll = long long;
 
 void solve() {
-    ll n,m;
-    cin>>n>>m;
-    vector<ll> odd,even;
-    ll sum = 0;
-    for(auto i=1; i<=n; i++){
-        ll el;
-        cin>>el;
-        if(i%2 == 1) odd.push_back(el);
-        else even.push_back(el);
-        sum += el;
-    }
+    int n,k;
+    cin>>n>>k;
+    vector<int> arr(n);
+    for(auto &x : arr) cin>>x;
+    int idx;
+    cin>>idx;
+    idx = idx-1;
+    int flip = 0;
 
-    sort(odd.begin(), odd.end());
-    sort(even.begin(), even.end());
+    int l = 0, r = n-1;
+    while(l <= idx || r >= idx){
+        while(l < idx && (((flip % 2 == 0) && arr[l] == arr[idx]) || 
+                ((flip % 2 == 1) && arr[l] != arr[idx]))) l++;
+        
+        while(r > idx && (((flip % 2 == 0) && arr[r] == arr[idx]) || 
+                ((flip % 2 == 1) && arr[r] != arr[idx]))) r--;
 
-    ll o = (ll)odd.size() - 1;
-    ll e = (ll)even.size() - 1;
-
-    vector<ll> queries(m);
-    for(auto &x : queries) cin>>x;
-
-    for(auto q : queries){
-        // odd query
-        if(q % 2 == 1){
-            if(o < 0) continue;
-            if(odd[o] >= 0 || o == odd.size() - 1) sum -= odd[o--];
+        bool changed = false;
+        if(l < idx){
+            flip++;
+            changed = true;
+            l++;
         }
-        // even query
-        else{
-            if(e < 0) continue;
-            if(even[e] >= 0 || e == even.size() - 1) sum -= even[e--];
+        else if(l == idx){
+            if(flip % 2 == 1){
+                flip++;
+                changed = true;
+            }
+            l++;
+        }
+
+        if(r > idx){
+            if(!changed) flip++;
+            r--;
+        }
+        else if(r == idx){
+            if(flip % 2 == 1){
+                if(!changed) flip++;
+            }
+            r--;
         }
     }
-
-    cout<<sum<<endl;
-
+    cout<<flip<<endl;
 }
 
 int main(){
